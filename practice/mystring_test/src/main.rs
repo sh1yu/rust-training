ee293a1afa27fa71b02cf397b6a7c816516dbd6b
pengshiyu.cc <pengshiyu.cc@bytedance.com>
2022-12-01 23:49:40 +0800

    add project vtable_test、deref_test、memalloc_test、borrow_test、mystring_test、async_trait_test

---
+++ practice/mystring_test/src/main.rs
@@ -0,0 +1,91 @@
+use std::{fmt, ops::Deref, str};
+const MINI_STRING_MAX_LEN: usize = 30;
+
+struct MiniString {
+    len: u8,
+    data: [u8; MINI_STRING_MAX_LEN],
+}
+
+impl MiniString {
+    fn new(v: impl AsRef<str>) -> Self {
+        let bytes = v.as_ref().as_bytes();
+        let len = bytes.len();
+        let mut data = [0u8; MINI_STRING_MAX_LEN];
+        data[..len].copy_from_slice(bytes);
+        Self {
+            len: len as u8,
+            data: data,
+        }
+    }
+}
+
+impl Deref for MiniString {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        // 由于生成 MiniString 的接口是隐藏的，它只能来自字符串，所以下面这行是安全的
+        str::from_utf8(&self.data[..self.len as usize]).unwrap()
+        // 也可以直接用 unsafe 版本
+        // unsafe { str::from_utf8_unchecked(&self.data[..self.len as usize])
+    }
+}
+
+impl fmt::Debug for MiniString {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // 这里由于实现了 Deref trait，可以直接得到一个 &str 输出
+        write!(f, "{}", self.deref())
+    }
+}
+
+#[derive(Debug)]
+enum MyString {
+    Inline(MiniString),
+    Standard(String),
+}
+
+impl Deref for MyString {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        match *self {
+            MyString::Inline(ref v) => v.deref(),
+            MyString::Standard(ref v) => v.deref(),
+        }
+    }
+}
+
+impl From<&str> for MyString {
+    fn from(s: &str) -> Self {
+        match s.len() > MINI_STRING_MAX_LEN {
+            true => Self::Standard(s.to_owned()),
+            _ => Self::Inline(MiniString::new(s)),
+        }
+    }
+}
+impl fmt::Display for MyString {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.deref())
+    }
+}
+
+fn main() {
+    let len1 = std::mem::size_of::<MyString>();
+    let len2 = std::mem::size_of::<MiniString>();
+    println!("Len: MyString {}, MiniString {}", len1, len2);
+
+    let s1: MyString = "hello world".into();
+    let s2: MyString = "这是一个超过了三十个字节的很长很长的字符串".into();
+
+    println!("s1: {:?}, s2: {:?}", s1, s2);
+    println!(
+        "s1: {}({} bytes, {} chars), s2: {}({} bytes, {} chars)",
+        s1,
+        s1.len(),
+        s1.chars().count(),
+        s2,
+        s2.len(),
+        s2.chars().count()
+    );
+
+    assert!(s1.ends_with("world"));
+    assert!(s2.starts_with("这"));
+}
